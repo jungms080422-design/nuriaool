496380ebe526517e0c8828bf9e5fba14e5cf3e63
jungms080422-design <jungms080422@gmail.com>
2025-11-02 16:13:59 +0900

    Update teamproject.py

--- pages/teamproject.py
+++ pages/teamproject.py
@@ -1,1 +1,10 @@
-
+LifeStats-Simulator/
+│
+├── app.py
+├── requirements.txt
+├── README.md
+└── data/
+    ├── 청소년_자살시도율.csv
+    ├── 청소년_우울증.csv
+    ├── 사회문제_위험_노출_정도_20251102160905.csv
+    └── 현재_흡연율_일반담배_관련__현재사용률__20251102155742.csv
